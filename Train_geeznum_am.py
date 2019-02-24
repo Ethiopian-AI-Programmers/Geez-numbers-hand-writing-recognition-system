a3e5a6beaa9fdb1926aa3b1baaf319103abbd360
Tesfamichael1074 <hope.miky1074@gmail.com>
2019-02-24 01:03:48 -0800

    added windows scripts

--- Train_geeznum_am.py
+++ Train_geeznum_am.py
@@ -2,12 +2,9 @@ from keras.models import Sequential
 from keras.layers import Convolution2D
 from keras.layers import MaxPooling2D
 from keras.layers import Flatten
-from keras.layers import Dropout
 from keras.layers import Dense
 from keras.preprocessing.image import ImageDataGenerator
-from keras.layers.core import Activation
-import cv2
-import numpy as np
+
 
 classifier = Sequential()
 
@@ -20,7 +17,7 @@ classifier.add(MaxPooling2D(pool_size = (2,2)))
 
 classifier.add(Flatten())
 classifier.add(Dense(128, activation = 'relu'))
-classifier.add(Dense(4, activation = 'softmax'))
+classifier.add(Dense(9, activation = 'softmax'))
 
 classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
 
@@ -34,7 +31,7 @@ test_datagen = ImageDataGenerator(rescale = 1./255)
 
 training_set  = train_datagen.flow_from_directory('am_numbers',
                                                 target_size=(28,28),
-                                                batch_size=50,
+                                                batch_size=32,
                                                 class_mode='categorical')
 
 
@@ -43,12 +40,7 @@ training_set  = train_datagen.flow_from_directory('am_numbers',
 
 classifier.fit_generator(training_set,
                         epochs = 10,
-                        steps_per_epoch=400
+                        steps_per_epoch=900
                         )
-'''
-ima = cv2.imread('nnnn.jpg')
-ima = np.reshape(ima,[1,28,28,3])
-class1 = classifier.predict_classes(ima)
-print(class1)
-'''
-classifier.save('myclass_num.h5')
+
+classifier.save('myclass_num1.h5')
